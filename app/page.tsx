2512b2684af9c2cd14f80146c2dd8a71eda77951
Andres <21141606+andresscode@users.noreply.github.com>
2024-01-17 16:47:10 -0500

    config: install shadcn button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p>Hello, World!</p>;
+  return (
+    <>
+      <p>Hello, World!</p>
+      <Button>Click me</Button>
+    </>
+  );
 }
