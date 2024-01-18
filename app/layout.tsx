f7bb65b499a8ceed0f7d09fd8227ff7244a9305f
Andres <21141606+andresscode@users.noreply.github.com>
2024-01-17 21:41:04 -0500

    feat: add create course form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ToastProvider from "@/components/providers/toaster-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <ToastProvider />
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
