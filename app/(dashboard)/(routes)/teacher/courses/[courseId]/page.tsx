ea338d179dad12a74a35ee545d473c75ed6c780f
Andres <21141606+andresscode@users.noreply.github.com>
2024-01-18 12:38:23 -0500

    feat: create initial title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import TitleForm from "./_components/title-form";
 
 export default async function CoursePage({
   params,
@@ -48,6 +49,7 @@ export default async function CoursePage({
             <IconBadge icon={LayoutDashboard} />
             <h2 className="text-xl">Customize your course</h2>
           </div>
+          <TitleForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
